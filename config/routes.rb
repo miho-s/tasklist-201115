da4566c6b5d6ebd5e66b03919dbc706c501c2a86
miho.s <s.mihoxx@gmail.com>
2020-11-17 12:58:46 +0000

    commit1117

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   get 'users/index'
-  get 'users/show'
   get 'users/new'
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
