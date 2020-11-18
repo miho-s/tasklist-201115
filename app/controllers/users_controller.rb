33d1db80c0b19576e8f54bb50c622c318e03de74
miho.s <s.mihoxx@gmail.com>
2020-11-18 12:16:19 +0000

    forheroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index]
   
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
 
   def new
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/index'
   get 'users/new'
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -14,7 +13,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   resources :tasks, only: [:create, :destroy]
   
 end
