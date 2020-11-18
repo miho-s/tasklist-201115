2111ad4d767bf2c1bbc27e669b19ee8192bf1537
miho.s <s.mihoxx@gmail.com>
2020-11-18 13:54:56 +0000

    forhiroku2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy, :update, :show, :edit]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   
   def index
@@ -56,10 +55,6 @@ class TasksController < ApplicationController
 
 
 private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   # Strong Parameter
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
@@ -14,6 +10,5 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
-  resources :tasks, only: [:create, :destroy]
   
 end
